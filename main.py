e37c956abdb0ef6ab74e3b58321760ee95b0b048
gzpaitch <gzpaitch@users.noreply.github.com>
2023-08-06 22:21:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/news")
+async def get_news():
+    response = requests.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY')
+    return response.json()
+
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# async def root():
+#     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
